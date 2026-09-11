b53dbff16c777c99956199ddfac691db2afb295a
Rekha Bhatt <312251687+bhattrekha798-boop@users.noreply.github.com>
2026-09-11 12:08:42 +0530

    Added AT content calculation

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -20,6 +20,7 @@ g_count = dna.count("G")
 c_count = dna.count("C")
 
 gc_content = ((g_count + c_count) / length) * 100
+at_content = ((a_count + t_count) / length) * 100
 
 print("\nDNA Sequence Analysis")
 print("---------------------")
@@ -29,3 +30,4 @@ print("T count:", t_count)
 print("G count:", g_count)
 print("C count:", c_count)
 print("GC content:", round(gc_content, 2), "%")
+print("AT content:", round(at_content, 2), "%")
